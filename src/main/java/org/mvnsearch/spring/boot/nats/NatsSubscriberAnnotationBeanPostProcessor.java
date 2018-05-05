d2c5291f8e3b37730b33d70843e794f0140feb80
linux_china <linux_china@hotmail.com>
2018-05-05 15:51:48 -0700

    implement subscribe logic

--- src/main/java/org/mvnsearch/spring/boot/nats/NatsSubscriberAnnotationBeanPostProcessor.java
+++ src/main/java/org/mvnsearch/spring/boot/nats/NatsSubscriberAnnotationBeanPostProcessor.java
@@ -1,30 +1,37 @@
 package org.mvnsearch.spring.boot.nats;
 
+import io.nats.client.AsyncSubscription;
+import io.nats.client.Connection;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
-import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.MethodIntrospector;
 import org.springframework.core.Ordered;
+import org.springframework.core.annotation.AnnotationUtils;
+
+import java.lang.reflect.Method;
+import java.util.*;
 
 /**
- * NatsSubscriber annotation bean post processor
- * https://github.com/spring-projects/spring-kafka/blob/master/spring-kafka/src/main/java/org/springframework/kafka/annotation/KafkaListenerAnnotationBeanPostProcessor.java
+ * NatsSubscriber annotation bean post processor: register subscriber on NATS connection
  *
  * @author linux_china
  */
-public class NatsSubscriberAnnotationBeanPostProcessor implements BeanPostProcessor, Ordered, BeanFactoryAware, SmartInitializingSingleton {
-
+public class NatsSubscriberAnnotationBeanPostProcessor implements BeanPostProcessor, Ordered, BeanFactoryAware, DisposableBean {
     private BeanFactory beanFactory;
+    private List<AsyncSubscription> subscriptions = new ArrayList<>();
 
     @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        this.beanFactory = beanFactory;
+    public int getOrder() {
+        return LOWEST_PRECEDENCE;
     }
 
     @Override
-    public void afterSingletonsInstantiated() {
-
+    public void setBeanFactory(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
     }
 
     @Override
@@ -33,13 +40,58 @@ public class NatsSubscriberAnnotationBeanPostProcessor implements BeanPostProces
     }
 
     @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+    public Object postProcessAfterInitialization(final Object bean, final String beanName) throws BeansException {
+        Class<?> targetClass = AopUtils.getTargetClass(bean);
+        Map<Method, Set<NatsSubscriber>> annotatedMethods = MethodIntrospector.selectMethods(targetClass,
+                (MethodIntrospector.MetadataLookup<Set<NatsSubscriber>>) method -> {
+                    Set<NatsSubscriber> listenerMethods = findSubscriberAnnotations(method);
+                    return (!listenerMethods.isEmpty() ? listenerMethods : null);
+                });
+        // Non-empty set of subscribed methods
+        if (!annotatedMethods.isEmpty()) {
+            for (Map.Entry<Method, Set<NatsSubscriber>> entry : annotatedMethods.entrySet()) {
+                Method method = entry.getKey();
+                for (NatsSubscriber listener : entry.getValue()) {
+                    processNatsSubscriber(listener, method, bean, beanName);
+                }
+            }
+        }
         return bean;
     }
 
-    @Override
-    public int getOrder() {
-        return LOWEST_PRECEDENCE;
+
+    /*
+     * AnnotationUtils.getRepeatableAnnotations does not look at interfaces
+     */
+    private Set<NatsSubscriber> findSubscriberAnnotations(Method method) {
+        Set<NatsSubscriber> subscribers = new HashSet<>();
+        NatsSubscriber natsSubscriber = AnnotationUtils.findAnnotation(method, NatsSubscriber.class);
+        if (natsSubscriber != null) {
+            subscribers.add(natsSubscriber);
+        }
+        NatsSubscribers natsSubscribers = AnnotationUtils.findAnnotation(method, NatsSubscribers.class);
+        if (natsSubscribers != null) {
+            subscribers.addAll(Arrays.asList(natsSubscribers.value()));
+        }
+        return subscribers;
     }
 
+    private void processNatsSubscriber(NatsSubscriber natsSubscriber, Method method, Object bean, String beanName) {
+        Connection nats = beanFactory.getBean(Connection.class);
+        AsyncSubscription subscription = nats.subscribe(natsSubscriber.subject(), msg -> {
+            try {
+                method.invoke(bean, msg);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        subscriptions.add(subscription);
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        for (AsyncSubscription subscription : subscriptions) {
+            subscription.close();
+        }
+    }
 }
