8207dc0a1b6960a58a3740665c6ff7f9cdc16ad8
linux_china <libing.chen@gmail.com>
2018-12-18 15:10:31 -0800

    add ngs support with jwt & nkey support

--- src/main/java/org/mvnsearch/spring/boot/nats/NatsAutoConfiguration.java
+++ src/main/java/org/mvnsearch/spring/boot/nats/NatsAutoConfiguration.java
@@ -1,12 +1,12 @@
 package org.mvnsearch.spring.boot.nats;
 
-import io.nats.client.Connection;
-import io.nats.client.Nats;
-import io.nats.client.Options;
+import io.nats.client.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
 
 /**
  * NATS auto configuration
@@ -22,7 +22,45 @@ public class NatsAutoConfiguration {
     @Bean(destroyMethod = "close")
     public Connection nats() throws Exception {
         Options.Builder builder = new Options.Builder();
-        builder.server(properties.getUrl()).token(properties.getToken());
+        builder.server(properties.getUrl());
+        if (properties.getJwtToken() != null && properties.getNkeyToken() != null) {
+            builder.authHandler(new AuthHandler() {
+                @Override
+                public byte[] sign(byte[] nonce) {
+                    try {
+                        char[] keyChars = this.getNkeyChars();
+                        NKey nkey = NKey.fromSeed(keyChars);
+                        byte[] sig = nkey.sign(nonce);
+                        nkey.clear();
+                        return sig;
+                    } catch (Exception exp) {
+                        throw new IllegalStateException("problem signing nonce", exp);
+                    }
+                }
+
+                @Override
+                public char[] getID() {
+                    try {
+                        char[] keyChars = this.getNkeyChars();
+                        NKey nkey = NKey.fromSeed(keyChars);
+                        char[] pubKey = nkey.getPublicKey();
+                        nkey.clear();
+                        return pubKey;
+                    } catch (Exception exp) {
+                        throw new IllegalStateException("problem getting public key", exp);
+                    }
+                }
+
+                @Override
+                public char[] getJWT() {
+                    return properties.getJwtToken().toCharArray();
+                }
+
+                private char[] getNkeyChars() throws IOException {
+                    return properties.getNkeyToken().toCharArray();
+                }
+            });
+        }
         return Nats.connect(builder.build());
     }
 
