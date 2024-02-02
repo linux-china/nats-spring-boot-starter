064e113e62b36438fd553758070cfa4d245bb23a
Libing Chen <libing.chen@gmail.com>
2024-02-02 23:32:31 +0800

    app: NatsExchangeProxyFactory

---
+++ src/main/java/org/mvnsearch/spring/boot/nats/configuration/NatsExchangeProxyFactory.java
@@ -0,0 +1,13 @@
+package org.mvnsearch.spring.boot.nats.configuration;
+
+import io.nats.client.Connection;
+
+import java.lang.reflect.Proxy;
+
+public class NatsExchangeProxyFactory {
+
+    public static  <T> T buildStub(Connection nc, Class<T> clazz) {
+        NatsServiceInvocationHandler handler = new NatsServiceInvocationHandler(nc, clazz);
+        return (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{clazz}, handler);
+    }
+}
