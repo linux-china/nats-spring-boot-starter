894f8ab4b37e355fd005f1cb3fa38ad8e75683a8
Libing Chen <libing.chen@gmail.com>
2024-02-02 23:30:22 +0800

    test: MathService

---
+++ src/test/java/org/mvnsearch/spring/boot/nats/client/MathService.java
@@ -0,0 +1,13 @@
+package org.mvnsearch.spring.boot.nats.client;
+
+import org.mvnsearch.spring.boot.nats.annotation.NatsExchange;
+import org.mvnsearch.spring.boot.nats.annotation.ServiceExchange;
+import reactor.core.publisher.Mono;
+
+@NatsExchange("nats://localhost:4222")
+public interface MathService {
+
+  @ServiceExchange("minmax.min")
+  Mono<Integer> min(String text);
+
+}
