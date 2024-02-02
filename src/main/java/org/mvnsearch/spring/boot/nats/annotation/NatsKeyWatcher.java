ca3d5b302f1202b1476344d37e0acd5a46520d08
Libing Chen <libing.chen@gmail.com>
2024-02-02 23:32:07 +0800

    app: NatsKeyWatcher

---
+++ src/main/java/org/mvnsearch/spring/boot/nats/annotation/NatsKeyWatcher.java
@@ -0,0 +1,14 @@
+package org.mvnsearch.spring.boot.nats.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface NatsKeyWatcher {
+    String key();
+
+    String bucket();
+
+    String format() default "json";
+}
