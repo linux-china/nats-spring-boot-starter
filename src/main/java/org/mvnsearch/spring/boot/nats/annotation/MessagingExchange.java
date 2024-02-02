be432154b24d70524d74690f938d61f2d32be1a9
Libing Chen <libing.chen@gmail.com>
2024-02-02 23:31:30 +0800

    app: MessagingExchange annotation

---
+++ src/main/java/org/mvnsearch/spring/boot/nats/annotation/MessagingExchange.java
@@ -0,0 +1,12 @@
+package org.mvnsearch.spring.boot.nats.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MessagingExchange {
+    String value();
+
+    String format() default "json";
+}
