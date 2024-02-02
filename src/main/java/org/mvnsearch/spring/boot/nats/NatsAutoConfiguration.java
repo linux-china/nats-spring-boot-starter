0e12a9f9fc7edd4e4f005b82b00ca69ade0ea581
Libing Chen <libing.chen@gmail.com>
2024-02-02 23:34:04 +0800

    chore: add more bean processors

--- src/main/java/org/mvnsearch/spring/boot/nats/NatsAutoConfiguration.java
+++ src/main/java/org/mvnsearch/spring/boot/nats/NatsAutoConfiguration.java
@@ -1,12 +1,10 @@
 package org.mvnsearch.spring.boot.nats;
 
-import io.nats.client.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.mvnsearch.spring.boot.nats.configuration.NatsDurableBeanPostProcessor;
+import org.mvnsearch.spring.boot.nats.configuration.NatsServiceBeanPostProcessor;
+import org.mvnsearch.spring.boot.nats.configuration.NatsSubscriberAnnotationBeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.IOException;
 
 /**
  * NATS auto configuration
@@ -14,68 +12,25 @@ import java.io.IOException;
  * @author linux_china
  */
 @Configuration
-@EnableConfigurationProperties(NatsProperties.class)
 public class NatsAutoConfiguration {
-    @Autowired
-    private NatsProperties properties;
 
-    @Bean(destroyMethod = "close")
-    public Connection nats() throws Exception {
-        Options.Builder builder = new Options.Builder();
-        builder.server(properties.getUrl());
-        if (properties.getJwtToken() != null && properties.getNkeyToken() != null) {
-            builder.authHandler(new AuthHandler() {
-                @Override
-                public byte[] sign(byte[] nonce) {
-                    try {
-                        char[] keyChars = this.getNkeyChars();
-                        NKey nkey = NKey.fromSeed(keyChars);
-                        byte[] sig = nkey.sign(nonce);
-                        nkey.clear();
-                        return sig;
-                    } catch (Exception exp) {
-                        throw new IllegalStateException("problem signing nonce", exp);
-                    }
-                }
+  @Bean
+  public NatsSubscriberAnnotationBeanPostProcessor natsSubscriberAnnotationBeanPostProcessor() {
+    return new NatsSubscriberAnnotationBeanPostProcessor();
+  }
 
-                @Override
-                public char[] getID() {
-                    try {
-                        char[] keyChars = this.getNkeyChars();
-                        NKey nkey = NKey.fromSeed(keyChars);
-                        char[] pubKey = nkey.getPublicKey();
-                        nkey.clear();
-                        return pubKey;
-                    } catch (Exception exp) {
-                        throw new IllegalStateException("problem getting public key", exp);
-                    }
-                }
+  @Bean
+  public NatsServiceBeanPostProcessor natsServiceBeanPostProcessor() {
+    return new NatsServiceBeanPostProcessor();
+  }
 
-                @Override
-                public char[] getJWT() {
-                    return properties.getJwtToken().toCharArray();
-                }
+  @Bean
+  public NatsDurableBeanPostProcessor natsDurableBeanPostProcessor() {
+    return new NatsDurableBeanPostProcessor();
+  }
 
-                private char[] getNkeyChars() throws IOException {
-                    return properties.getNkeyToken().toCharArray();
-                }
-            });
-        }
-        return Nats.connect(builder.build());
-    }
-
-    @Bean
-    public NatsSubscriberAnnotationBeanPostProcessor natsSubscriberAnnotationBeanPostProcessor() {
-        return new NatsSubscriberAnnotationBeanPostProcessor();
-    }
-
-    @Bean
-    public NatsEndpoint natsEndpoint() {
-        return new NatsEndpoint();
-    }
-
-    @Bean
-    public NatsHealthIndicator natsHealthIndicator() {
-        return new NatsHealthIndicator();
-    }
+  @Bean
+  public NatsHealthIndicator natsHealthIndicator() {
+    return new NatsHealthIndicator();
+  }
 }
