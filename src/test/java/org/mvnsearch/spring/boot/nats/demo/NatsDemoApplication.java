7f876a462a7973abd1a282096dbb39c62bae3624
linux_china <libing.chen@gmail.com>
2024-02-03 11:27:57 +0800

    feat: add AppInstanceOnlyMessageHandler

--- src/test/java/org/mvnsearch/spring/boot/nats/demo/NatsDemoApplication.java
+++ src/test/java/org/mvnsearch/spring/boot/nats/demo/NatsDemoApplication.java
@@ -1,9 +1,11 @@
 package org.mvnsearch.spring.boot.nats.demo;
 
 import io.nats.client.Message;
+import org.mvnsearch.spring.boot.nats.AppInstanceOnlyMessageHandler;
 import org.mvnsearch.spring.boot.nats.annotation.NatsSubscriber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * NATS demo application
@@ -11,14 +13,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author linux_china
  */
 @SpringBootApplication
-public class NatsDemoApplication {
+public class NatsDemoApplication implements AppInstanceOnlyMessageHandler {
 
-  public static void main(String[] args) {
-    SpringApplication.run(NatsDemoApplication.class, args);
-  }
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(NatsDemoApplication.class, args);
+        System.out.println("app:" + run.getId());
+    }
 
-  @NatsSubscriber(subject = "subject1")
-  public void handler(Message msg) {
-    System.out.println(msg.getSubject());
-  }
+    @NatsSubscriber(subject = "subject1")
+    public void handler(Message msg) {
+        System.out.println(msg.getSubject());
+    }
+
+    @Override
+    public void onMessage(Message msg) throws InterruptedException {
+        System.out.println("Received message from:" + msg.getSubject());
+    }
 }
