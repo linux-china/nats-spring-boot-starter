84e4c0632334030ef320ebb586e2c0746fa3d4dc
linux_china <libing.chen@gmail.com>
2024-02-03 09:52:45 +0800

    test: OnlineUserComponent

---
+++ src/test/java/org/mvnsearch/spring/boot/nats/demo/component/OnlineUserComponent.java
@@ -0,0 +1,20 @@
+package org.mvnsearch.spring.boot.nats.demo.component;
+
+import org.mvnsearch.spring.boot.nats.annotation.NatsDurableComponent;
+import org.mvnsearch.spring.boot.nats.annotation.NatsKeyWatcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@NatsDurableComponent
+public class OnlineUserComponent {
+    private static final Logger logger = LoggerFactory.getLogger(OnlineUserComponent.class);
+    private int onlineUserCount;
+
+    @NatsKeyWatcher(bucket = "bucket", key = "online.user.count")
+    public void setOnlineUserCount(int count) {
+        logger.info("Online user count: " + count);
+        this.onlineUserCount = count;
+    }
+}
