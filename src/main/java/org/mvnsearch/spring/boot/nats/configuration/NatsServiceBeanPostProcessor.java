d86f3e8593a95e7d83460c1802fb7ae6f4f965c3
Libing Chen <libing.chen@gmail.com>
2024-03-17 23:56:56 +0800

    chore: add messagingNats bean

--- src/main/java/org/mvnsearch/spring/boot/nats/configuration/NatsServiceBeanPostProcessor.java
+++ src/main/java/org/mvnsearch/spring/boot/nats/configuration/NatsServiceBeanPostProcessor.java
@@ -2,6 +2,7 @@ package org.mvnsearch.spring.boot.nats.configuration;
 
 import io.nats.client.Connection;
 import io.nats.service.*;
+import org.mvnsearch.spring.boot.nats.MessagingNats;
 import org.mvnsearch.spring.boot.nats.NatsContextAware;
 import org.mvnsearch.spring.boot.nats.annotation.NatsService;
 import org.mvnsearch.spring.boot.nats.serialization.SerializationUtil;
@@ -26,132 +27,135 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class NatsServiceBeanPostProcessor implements BeanPostProcessor, DisposableBean {
-    private static final Logger logger = LoggerFactory.getLogger(NatsServiceBeanPostProcessor.class);
+  private static final Logger logger = LoggerFactory.getLogger(NatsServiceBeanPostProcessor.class);
 
-    @Autowired
-    @Lazy
-    private Connection nc;
-    @Autowired
-    @Lazy
-    private NatsContextAware natsContextAware;
-    @Autowired
-    @Lazy
-    private Environment env;
-    private final List<Service> natsServices = new ArrayList<>();
+  @Autowired
+  @Lazy
+  private Connection nc;
+  @Autowired
+  @Lazy
+  private MessagingNats messagingNats;
+  @Autowired
+  @Lazy
+  private NatsContextAware natsContextAware;
+  @Autowired
+  @Lazy
+  private Environment env;
+  private final List<Service> natsServices = new ArrayList<>();
 
 
-    @Override
-    public void destroy() throws Exception {
-        for (Service natsService : natsServices) {
+  @Override
+  public void destroy() throws Exception {
+    for (Service natsService : natsServices) {
+      try {
+        natsService.stop(true);
+      } catch (Exception ignore) {
+
+      }
+    }
+  }
+
+  @Override
+  public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+    Class<?> clazz = bean.getClass();
+    NatsService natsService = AnnotationUtils.findAnnotation(clazz, NatsService.class);
+    if (natsService != null) {
+      Service service = registerNatsService(bean, clazz, natsService);
+      natsServices.add(service);
+    }
+    return bean;
+  }
+
+  public Service registerNatsService(Object bean, Class<?> clazz, NatsService natsService) {
+    // NATS Service builder
+    ServiceBuilder serviceBuilder = Service.builder()
+      .name(natsService.name())
+      .version(natsService.version())
+      .description(natsService.description().trim());
+    // service group
+    Group serviceGroup = null;
+    MessageMapping requestMapping4Class = AnnotationUtils.findAnnotation(clazz, MessageMapping.class);
+    if (requestMapping4Class != null) {
+      serviceGroup = new Group(requestMapping4Class.value()[0]);
+    }
+    // scan handler
+    for (Method method : clazz.getDeclaredMethods()) {
+      MessageMapping requestMapping4Method = AnnotationUtils.findAnnotation(method, MessageMapping.class);
+      if (requestMapping4Method != null) {
+        // endpoint metadata
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("appInstanceSubject", natsContextAware.getSubjectNameForAppInstance());
+        metadata.put("appName", env.getProperty("spring.application.name", "unknown-app"));
+        metadata.put("serverPort", env.getProperty("server.port", "0"));
+        metadata.put("managementServerPort", env.getProperty("management.server.port", "0"));
+        try {
+          String serverIp = getServerIp();
+          if (serverIp != null) {
+            metadata.put("serverIp", serverIp);
+          }
+        } catch (Exception ignore) {
+
+        }
+        // construct service endpoint
+        ServiceEndpoint serviceEndpoint = ServiceEndpoint.builder()
+          .endpointName(requestMapping4Method.value()[0])
+          .group(serviceGroup)
+          .endpointMetadata(metadata)
+          .handler(msg -> {
             try {
-                natsService.stop(true);
-            } catch (Exception ignore) {
-
+              Object param = msg;
+              Class<?> paramType = method.getParameterTypes()[0];
+              if (paramType == String.class) {
+                param = new String(msg.getData(), StandardCharsets.UTF_8);
+              } else if (paramType != ServiceMessage.class) {
+                param = SerializationUtil.convert(msg.getData(), paramType);
+              }
+              Object result = ReflectionUtils.invokeMethod(method, bean, param);
+              msg.respond(nc, SerializationUtil.toBytes(result));
+            } catch (Exception e) {
+              String fullName = clazz.getCanonicalName() + "." + method.getName();
+              logger.error("NATS-001500: failed to call NATS service: " + fullName, e);
+              msg.respond(nc, "error: " + e.getMessage());
             }
-        }
+          })
+          .build();
+        // add service endpoint
+        serviceBuilder.addServiceEndpoint(serviceEndpoint);
+      }
     }
+    Service service = serviceBuilder.connection(nc).build();
+    service.startService();
+    return service;
+  }
 
-    @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        Class<?> clazz = bean.getClass();
-        NatsService natsService = AnnotationUtils.findAnnotation(clazz, NatsService.class);
-        if (natsService != null) {
-            Service service = registerNatsService(bean, clazz, natsService);
-            natsServices.add(service);
+
+  public String getServerIp() throws Exception {
+    String serverIp = null;
+    Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+    while (networkInterfaces.hasMoreElements()) {
+      NetworkInterface networkInterface = networkInterfaces.nextElement();
+      Enumeration<?> ee = networkInterface.getInetAddresses();
+      while (ee.hasMoreElements()) {
+        InetAddress inetAddress = (InetAddress) ee.nextElement();
+        if (!inetAddress.isLoopbackAddress()) {
+          if (inetAddress instanceof Inet4Address) {
+            serverIp = inetAddress.getHostAddress();
+            break;
+          }
         }
-        return bean;
+      }
     }
-
-    public Service registerNatsService(Object bean, Class<?> clazz, NatsService natsService) {
-        // NATS Service builder
-        ServiceBuilder serviceBuilder = Service.builder()
-                .name(natsService.name())
-                .version(natsService.version())
-                .description(natsService.description().trim());
-        // service group
-        Group serviceGroup = null;
-        MessageMapping requestMapping4Class = AnnotationUtils.findAnnotation(clazz, MessageMapping.class);
-        if (requestMapping4Class != null) {
-            serviceGroup = new Group(requestMapping4Class.value()[0]);
+    if (serverIp == null) {
+      List<String> dnsServers = Arrays.asList("1.1.1.1", "8.8.8.8");
+      for (String dnsServer : dnsServers) {
+        try (final DatagramSocket socket = new DatagramSocket()) {
+          socket.connect(InetAddress.getByName(dnsServer), 10002);
+          serverIp = socket.getLocalAddress().getHostAddress();
+          break;
         }
-        // scan handler
-        for (Method method : clazz.getDeclaredMethods()) {
-            MessageMapping requestMapping4Method = AnnotationUtils.findAnnotation(method, MessageMapping.class);
-            if (requestMapping4Method != null) {
-                // endpoint metadata
-                Map<String, String> metadata = new HashMap<>();
-                metadata.put("appInstanceSubject", natsContextAware.getSubjectNameForAppInstance());
-                metadata.put("appName", env.getProperty("spring.application.name", "unknown-app"));
-                metadata.put("serverPort", env.getProperty("server.port", "0"));
-                metadata.put("managementServerPort", env.getProperty("management.server.port", "0"));
-                try {
-                    String serverIp = getServerIp();
-                    if (serverIp != null) {
-                        metadata.put("serverIp", serverIp);
-                    }
-                } catch (Exception ignore) {
-
-                }
-                // construct service endpoint
-                ServiceEndpoint serviceEndpoint = ServiceEndpoint.builder()
-                        .endpointName(requestMapping4Method.value()[0])
-                        .group(serviceGroup)
-                        .endpointMetadata(metadata)
-                        .handler(msg -> {
-                            try {
-                                Object param = msg;
-                                Class<?> paramType = method.getParameterTypes()[0];
-                                if (paramType == String.class) {
-                                    param = new String(msg.getData(), StandardCharsets.UTF_8);
-                                } else if (paramType != ServiceMessage.class) {
-                                    param = SerializationUtil.convert(msg.getData(), paramType);
-                                }
-                                Object result = ReflectionUtils.invokeMethod(method, bean, param);
-                                msg.respond(nc, SerializationUtil.toBytes(result));
-                            } catch (Exception e) {
-                                String fullName = clazz.getCanonicalName() + "." + method.getName();
-                                logger.error("NATS-001500: failed to call NATS service: " + fullName, e);
-                                msg.respond(nc, "error: " + e.getMessage());
-                            }
-                        })
-                        .build();
-                // add service endpoint
-                serviceBuilder.addServiceEndpoint(serviceEndpoint);
-            }
-        }
-        Service service = serviceBuilder.connection(nc).build();
-        service.startService().join();
-        return service;
+      }
     }
-
-
-    public String getServerIp() throws Exception {
-        String serverIp = null;
-        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
-        while (networkInterfaces.hasMoreElements()) {
-            NetworkInterface networkInterface = networkInterfaces.nextElement();
-            Enumeration<?> ee = networkInterface.getInetAddresses();
-            while (ee.hasMoreElements()) {
-                InetAddress inetAddress = (InetAddress) ee.nextElement();
-                if (!inetAddress.isLoopbackAddress()) {
-                    if (inetAddress instanceof Inet4Address) {
-                        serverIp = inetAddress.getHostAddress();
-                        break;
-                    }
-                }
-            }
-        }
-        if (serverIp == null) {
-            List<String> dnsServers = Arrays.asList("1.1.1.1", "8.8.8.8");
-            for (String dnsServer : dnsServers) {
-                try (final DatagramSocket socket = new DatagramSocket()) {
-                    socket.connect(InetAddress.getByName(dnsServer), 10002);
-                    serverIp = socket.getLocalAddress().getHostAddress();
-                    break;
-                }
-            }
-        }
-        return serverIp;
-    }
+    return serverIp;
+  }
 
 }
