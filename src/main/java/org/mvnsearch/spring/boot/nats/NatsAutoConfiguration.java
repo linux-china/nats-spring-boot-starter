e16f1cce423e31eca0f3fa55ed3f8b5c2b0c3b68
Libing Chen <libing.chen@gmail.com>
2024-03-17 23:55:20 +0800

    feat: add MessagingNats

--- src/main/java/org/mvnsearch/spring/boot/nats/NatsAutoConfiguration.java
+++ src/main/java/org/mvnsearch/spring/boot/nats/NatsAutoConfiguration.java
@@ -1,11 +1,18 @@
 package org.mvnsearch.spring.boot.nats;
 
-import org.mvnsearch.spring.boot.nats.configuration.NatsDurableBeanPostProcessor;
+import io.nats.client.Connection;
 import org.mvnsearch.spring.boot.nats.configuration.AppInstanceOnlyMessageHandlerBeanPostProcessor;
+import org.mvnsearch.spring.boot.nats.configuration.NatsDurableBeanPostProcessor;
 import org.mvnsearch.spring.boot.nats.configuration.NatsServiceBeanPostProcessor;
 import org.mvnsearch.spring.boot.nats.configuration.NatsSubscriberAnnotationBeanPostProcessor;
+import org.mvnsearch.spring.boot.nats.services.NatsMessageHandlerCustomizer;
+import org.mvnsearch.spring.boot.nats.services.NatsServiceMessageHandler;
+import org.mvnsearch.spring.boot.nats.services.NatsStrategies;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.ReactiveMessageHandler;
 
 /**
  * NATS auto configuration
@@ -15,28 +22,42 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class NatsAutoConfiguration {
 
-    @Bean
-    public AppInstanceOnlyMessageHandlerBeanPostProcessor appInstanceOnlyMessageHandlerBeanPostProcessor() {
-        return new AppInstanceOnlyMessageHandlerBeanPostProcessor();
-    }
+  @Bean
+  public MessagingNats messagingNats(Connection nc, ReactiveMessageHandler messageHandler, NatsStrategies strategies) {
+    return new MessagingNats(new MediaType("text", "plain"), nc, messageHandler, strategies);
+  }
 
-    @Bean
-    public NatsSubscriberAnnotationBeanPostProcessor natsSubscriberAnnotationBeanPostProcessor() {
-        return new NatsSubscriberAnnotationBeanPostProcessor();
-    }
+  @Bean
+  public AppInstanceOnlyMessageHandlerBeanPostProcessor appInstanceOnlyMessageHandlerBeanPostProcessor() {
+    return new AppInstanceOnlyMessageHandlerBeanPostProcessor();
+  }
 
-    @Bean
-    public NatsServiceBeanPostProcessor natsServiceBeanPostProcessor() {
-        return new NatsServiceBeanPostProcessor();
-    }
+  @Bean
+  public NatsSubscriberAnnotationBeanPostProcessor natsSubscriberAnnotationBeanPostProcessor() {
+    return new NatsSubscriberAnnotationBeanPostProcessor();
+  }
 
-    @Bean
-    public NatsDurableBeanPostProcessor natsDurableBeanPostProcessor() {
-        return new NatsDurableBeanPostProcessor();
-    }
+  @Bean
+  public NatsServiceMessageHandler natsServiceMessageHandler(NatsStrategies natsStrategies,
+                                                             ObjectProvider<NatsMessageHandlerCustomizer> customizers) {
+    NatsServiceMessageHandler messageHandler = new NatsServiceMessageHandler();
+    messageHandler.setNatsStrategies(natsStrategies);
+    customizers.orderedStream().forEach((customizer) -> customizer.customize(messageHandler));
+    return messageHandler;
+  }
 
-    @Bean
-    public NatsHealthIndicator natsHealthIndicator() {
-        return new NatsHealthIndicator();
-    }
+  @Bean
+  public NatsServiceBeanPostProcessor natsServiceBeanPostProcessor() {
+    return new NatsServiceBeanPostProcessor();
+  }
+
+  @Bean
+  public NatsDurableBeanPostProcessor natsDurableBeanPostProcessor() {
+    return new NatsDurableBeanPostProcessor();
+  }
+
+  @Bean
+  public NatsHealthIndicator natsHealthIndicator() {
+    return new NatsHealthIndicator();
+  }
 }
