7c07fd323e71ab8fdd9a8cf3027e24d7283b5f7f
Libing Chen <libing.chen@gmail.com>
2024-03-23 16:13:48 +0800

    chore: add Integer, Double, Float, Boolean support for text/plain

--- src/main/java/org/mvnsearch/spring/boot/nats/serialization/SerializationUtil.java
+++ src/main/java/org/mvnsearch/spring/boot/nats/serialization/SerializationUtil.java
@@ -9,8 +9,23 @@ public class SerializationUtil {
   private static final ProtobufSerialization protobuf = new ProtobufSerialization();
 
   public static Object convert(byte[] bytes, Class<?> targetClass, String contentType) throws Exception {
-    if (targetClass == String.class || contentType.startsWith("text/")) {
-      return new String(bytes, StandardCharsets.UTF_8);
+    if (contentType.startsWith("text/")) {
+      String text = new String(bytes, StandardCharsets.UTF_8);
+      if (targetClass.isAssignableFrom(String.class)) {
+        return text;
+      } else if (targetClass.isAssignableFrom(Integer.class)) {
+        return Integer.parseInt(text);
+      } else if (targetClass.isAssignableFrom(Long.class)) {
+        return Long.parseLong(text);
+      } else if (targetClass.isAssignableFrom(Double.class)) {
+        return Double.parseDouble(text);
+      } else if (targetClass.isAssignableFrom(Float.class)) {
+        return Float.parseFloat(text);
+      } else if (targetClass.isAssignableFrom(Boolean.class)) {
+        return Boolean.parseBoolean(text);
+      } else {
+        return json.convert(bytes, targetClass);
+      }
     } else if (targetClass.isAssignableFrom(Message.class)) {
       return protobuf.convert(bytes, targetClass);
     } else {
