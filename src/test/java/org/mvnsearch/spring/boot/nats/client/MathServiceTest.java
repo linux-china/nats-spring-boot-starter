1a9a94becc8b660fae1a24689a359dbf11dca9e4
Libing Chen <libing.chen@gmail.com>
2024-03-23 16:59:52 +0800

    test: add testPost()

--- src/test/java/org/mvnsearch/spring/boot/nats/client/MathServiceTest.java
+++ src/test/java/org/mvnsearch/spring/boot/nats/client/MathServiceTest.java
@@ -2,16 +2,34 @@ package org.mvnsearch.spring.boot.nats.client;
 
 import io.nats.client.Connection;
 import io.nats.client.Nats;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mvnsearch.spring.boot.nats.configuration.NatsExchangeProxyFactory;
 
 public class MathServiceTest {
+  private static Connection nc;
+  private static MathService mathService;
 
-    @Test
-    public void testServiceCall() throws Exception {
-        Connection nc = Nats.connect("nats://localhost:4222");
-        MathService userService = NatsExchangeProxyFactory.buildStub(nc, MathService.class);
-        Integer min = userService.min("1,2").block();
-        System.out.println(min);
-    }
+  @BeforeAll
+  public static void setUp() throws Exception {
+    nc = Nats.connect("nats://localhost:4222");
+    mathService = NatsExchangeProxyFactory.buildStub(nc, MathService.class);
+  }
+
+  @AfterAll
+  public static void tearDown() throws Exception {
+    nc.close();
+  }
+
+  @Test
+  public void testServiceCall() throws Exception {
+    Integer min = mathService.min("1,2").block();
+    System.out.println(min);
+  }
+
+  @Test
+  public void testPost() throws Exception {
+    mathService.post("Hello");
+  }
+
 }
