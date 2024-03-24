1e2f2e746b2f3de9a8edbd4041965ba45f8fbfe3
Libing Chen <libing.chen@gmail.com>
2024-03-24 13:25:39 +0800

    chore: add natsTemplate

--- src/test/java/org/mvnsearch/spring/boot/nats/demo/PortalController.java
+++ src/test/java/org/mvnsearch/spring/boot/nats/demo/PortalController.java
@@ -1,10 +1,14 @@
 package org.mvnsearch.spring.boot.nats.demo;
 
+import org.mvnsearch.spring.boot.nats.core.NatsTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PortalController {
+  @Autowired
+  private NatsTemplate natsTemplate;
   @GetMapping("/")
   public String index() {
     return "Hello Nats";
