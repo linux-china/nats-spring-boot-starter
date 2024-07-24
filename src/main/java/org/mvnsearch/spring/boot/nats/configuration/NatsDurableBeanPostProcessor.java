6a0461a34a4ec5314f1f78b1b7d4cd22947816a5
linux_china <libing.chen@gmail.com>
2024-07-24 20:10:17 +0800

    chore: add placeholder support ${xxx}

--- src/main/java/org/mvnsearch/spring/boot/nats/configuration/NatsDurableBeanPostProcessor.java
+++ src/main/java/org/mvnsearch/spring/boot/nats/configuration/NatsDurableBeanPostProcessor.java
@@ -12,9 +12,13 @@ import org.mvnsearch.spring.boot.nats.serialization.SerializationUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.beans.factory.config.EmbeddedValueResolver;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.lang.NonNull;
@@ -24,66 +28,81 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NatsDurableBeanPostProcessor implements BeanPostProcessor, DisposableBean, NatsDisposable {
-  private static final Logger logger = LoggerFactory.getLogger(NatsDurableBeanPostProcessor.class);
+public class NatsDurableBeanPostProcessor implements BeanFactoryAware, BeanPostProcessor, DisposableBean, NatsDisposable {
+    private static final Logger logger = LoggerFactory.getLogger(NatsDurableBeanPostProcessor.class);
+    private BeanFactory beanFactory;
+    private EmbeddedValueResolver resolver;
+    @Autowired
+    @Lazy
+    private Connection nc;
+    private final List<NatsKeyValueWatchSubscription> subscriptions = new ArrayList<>();
 
-  @Autowired
-  @Lazy
-  private Connection nc;
-  private final List<NatsKeyValueWatchSubscription> subscriptions = new ArrayList<>();
-
-  @Override
-  public void destroy() {
-    for (NatsKeyValueWatchSubscription subscription : subscriptions) {
-      try {
-        subscription.close();
-      } catch (Exception ignore) {
-
-      }
+    @Override
+    public void setBeanFactory(@NonNull BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+        this.resolver = new EmbeddedValueResolver((ConfigurableBeanFactory) beanFactory);
     }
-  }
 
+    @Override
+    public void destroy() {
+        for (NatsKeyValueWatchSubscription subscription : subscriptions) {
+            try {
+                subscription.close();
+            } catch (Exception ignore) {
 
-  @Override
-  public Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
-    Class<?> clazz = bean.getClass();
-    NatsDurableComponent natsDurableComponent = AnnotationUtils.findAnnotation(clazz, NatsDurableComponent.class);
-    if (natsDurableComponent != null) {
-      for (Method method : clazz.getDeclaredMethods()) {
-        NatsKeyWatcher watcher = AnnotationUtils.findAnnotation(method, NatsKeyWatcher.class);
-        if (watcher != null) {
-          try {
-            watchKey(nc, watcher, bean, method);
-          } catch (Exception ignore) {
-
-          }
+            }
         }
-      }
-
     }
-    return bean;
-  }
 
-  public void watchKey(Connection nc, NatsKeyWatcher natsKeyWatcher, Object bean, Method method) throws Exception {
-    NatsKeyValueWatchSubscription subscription = nc.keyValue(natsKeyWatcher.bucket()).watch(natsKeyWatcher.key(), new KeyValueWatcher() {
-      @Override
-      public void watch(KeyValueEntry keyValueEntry) {
-        try {
-          byte[] textValue = keyValueEntry.getValue();
-          Class<?> paramType = method.getParameterTypes()[0];
-          Object param = SerializationUtil.convert(textValue, paramType, "text/plain");
-          ReflectionUtils.invokeMethod(method, bean, param);
-        } catch (Exception e) {
-          logger.error("NATS-020500: failed to update watched key", e);
+
+    @Override
+    public Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
+        Class<?> clazz = bean.getClass();
+        NatsDurableComponent natsDurableComponent = AnnotationUtils.findAnnotation(clazz, NatsDurableComponent.class);
+        if (natsDurableComponent != null) {
+            for (Method method : clazz.getDeclaredMethods()) {
+                NatsKeyWatcher watcher = AnnotationUtils.findAnnotation(method, NatsKeyWatcher.class);
+                if (watcher != null) {
+                    try {
+                        watchKey(nc, watcher, bean, method);
+                    } catch (Exception ignore) {
+
+                    }
+                }
+            }
+
         }
-      }
+        return bean;
+    }
 
-      @Override
-      public void endOfData() {
+    public void watchKey(Connection nc, NatsKeyWatcher natsKeyWatcher, Object bean, Method method) throws Exception {
+        String bucket = natsKeyWatcher.bucket();
+        String key = natsKeyWatcher.key();
+        if (bucket.startsWith("${")) {
+            bucket = resolver.resolveStringValue(bucket);
+        }
+        if (key.startsWith("${")) {
+            key = resolver.resolveStringValue(key);
+        }
+        NatsKeyValueWatchSubscription subscription = nc.keyValue(bucket).watch(key, new KeyValueWatcher() {
+            @Override
+            public void watch(KeyValueEntry keyValueEntry) {
+                try {
+                    byte[] textValue = keyValueEntry.getValue();
+                    Class<?> paramType = method.getParameterTypes()[0];
+                    Object param = SerializationUtil.convert(textValue, paramType, "text/plain");
+                    ReflectionUtils.invokeMethod(method, bean, param);
+                } catch (Exception e) {
+                    logger.error("NATS-020500: failed to update watched key", e);
+                }
+            }
 
-      }
-    });
-    this.subscriptions.add(subscription);
-  }
+            @Override
+            public void endOfData() {
+
+            }
+        });
+        this.subscriptions.add(subscription);
+    }
 
 }
--- src/main/java/org/mvnsearch/spring/boot/nats/configuration/NatsSubscriberAnnotationBeanPostProcessor.java
+++ src/main/java/org/mvnsearch/spring/boot/nats/configuration/NatsSubscriberAnnotationBeanPostProcessor.java
@@ -13,6 +13,8 @@ import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.beans.factory.config.EmbeddedValueResolver;
 import org.springframework.core.MethodIntrospector;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.AnnotationUtils;
@@ -30,111 +32,122 @@ import java.util.*;
  * @author linux_china
  */
 public class NatsSubscriberAnnotationBeanPostProcessor implements BeanPostProcessor, Ordered, BeanFactoryAware, InitializingBean, DisposableBean, NatsDisposable {
-  private static final Logger log = LoggerFactory.getLogger(NatsSubscriberAnnotationBeanPostProcessor.class);
-  private BeanFactory beanFactory;
-  private final Map<NatsSubscriber, Dispatcher> subscriptions = new HashMap<>();
-  private Connection nc;
+    private static final Logger log = LoggerFactory.getLogger(NatsSubscriberAnnotationBeanPostProcessor.class);
+    private BeanFactory beanFactory;
+    private EmbeddedValueResolver resolver;
+    private final Map<NatsSubscriber, Dispatcher> subscriptions = new HashMap<>();
+    private Connection nc;
 
-  @Override
-  public void afterPropertiesSet() {
-    nc = beanFactory.getBean(Connection.class);
-  }
+    @Override
+    public void afterPropertiesSet() {
+        nc = beanFactory.getBean(Connection.class);
+    }
 
-  public Map<NatsSubscriber, Dispatcher> getSubscriptions() {
-    return subscriptions;
-  }
+    public Map<NatsSubscriber, Dispatcher> getSubscriptions() {
+        return subscriptions;
+    }
 
-  @Override
-  public int getOrder() {
-    return LOWEST_PRECEDENCE;
-  }
+    @Override
+    public int getOrder() {
+        return LOWEST_PRECEDENCE;
+    }
 
-  @Override
-  public void setBeanFactory(@NonNull BeanFactory beanFactory) {
-    this.beanFactory = beanFactory;
-  }
+    @Override
+    public void setBeanFactory(@NonNull BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+        this.resolver = new EmbeddedValueResolver((ConfigurableBeanFactory) beanFactory);
+    }
 
-  @Override
-  public Object postProcessAfterInitialization(@NonNull final Object bean, @NonNull final String beanName) throws BeansException {
-    Class<?> targetClass = AopUtils.getTargetClass(bean);
-    Map<Method, Set<NatsSubscriber>> annotatedMethods = MethodIntrospector.selectMethods(targetClass,
-      (MethodIntrospector.MetadataLookup<Set<NatsSubscriber>>) method -> {
-        Set<NatsSubscriber> listenerMethods = findSubscriberAnnotations(method);
-        return (!listenerMethods.isEmpty() ? listenerMethods : null);
-      });
-    // Non-empty set of subscribed methods
-    if (!annotatedMethods.isEmpty()) {
-      try {
-        for (Map.Entry<Method, Set<NatsSubscriber>> entry : annotatedMethods.entrySet()) {
-          Method method = entry.getKey();
-          for (NatsSubscriber listener : entry.getValue()) {
-            processNatsSubscriber(listener, method, bean, beanName);
-          }
+    @Override
+    public Object postProcessAfterInitialization(@NonNull final Object bean, @NonNull final String beanName) throws BeansException {
+        Class<?> targetClass = AopUtils.getTargetClass(bean);
+        Map<Method, Set<NatsSubscriber>> annotatedMethods = MethodIntrospector.selectMethods(targetClass,
+                (MethodIntrospector.MetadataLookup<Set<NatsSubscriber>>) method -> {
+                    Set<NatsSubscriber> listenerMethods = findSubscriberAnnotations(method);
+                    return (!listenerMethods.isEmpty() ? listenerMethods : null);
+                });
+        // Non-empty set of subscribed methods
+        if (!annotatedMethods.isEmpty()) {
+            try {
+                for (Map.Entry<Method, Set<NatsSubscriber>> entry : annotatedMethods.entrySet()) {
+                    Method method = entry.getKey();
+                    for (NatsSubscriber listener : entry.getValue()) {
+                        processNatsSubscriber(listener, method, bean, beanName);
+                    }
+                }
+            } catch (Exception e) {
+                throw new BeanCreationException("Failed to construct NATS Subscriber handler", e);
+            }
         }
-      } catch (Exception e) {
-        throw new BeanCreationException("Failed to construct NATS Subscriber handler", e);
-      }
-    }
-    return bean;
-  }
-
-
-  /*
-   * AnnotationUtils.getRepeatableAnnotations does not look at interfaces
-   */
-  private Set<NatsSubscriber> findSubscriberAnnotations(Method method) {
-    Set<NatsSubscriber> subscribers = new HashSet<>();
-    NatsSubscriber natsSubscriber = AnnotationUtils.findAnnotation(method, NatsSubscriber.class);
-    if (natsSubscriber != null) {
-      subscribers.add(natsSubscriber);
-    }
-    NatsSubscribers natsSubscribers = AnnotationUtils.findAnnotation(method, NatsSubscribers.class);
-    if (natsSubscribers != null) {
-      subscribers.addAll(Arrays.asList(natsSubscribers.value()));
-    }
-    return subscribers;
-  }
-
-  private void processNatsSubscriber(NatsSubscriber natsSubscriber, Method method, Object bean, String beanName) throws NoSuchMethodException, IllegalAccessException {
-    //use method handler instead of reflection for performance
-    MethodHandle methodHandler = getMethodHandler(method);
-    MessageHandler messageHandler = msg -> {
-      try {
-        methodHandler.invoke(bean, msg);
-        // method.invoke(bean, msg);
-      } catch (Throwable e) {
-        throw new RuntimeException(e);
-      }
-    };
-    Dispatcher dispatcher;
-    if ("".equals(natsSubscriber.queueGroup())) {
-      dispatcher = nc.createDispatcher(messageHandler);
-      dispatcher.subscribe(natsSubscriber.subject());
-    } else {
-      dispatcher = nc.createDispatcher(messageHandler);
-      dispatcher.subscribe(natsSubscriber.subject(), natsSubscriber.queueGroup());
-    }
-    subscriptions.put(natsSubscriber, dispatcher);
-  }
-
-  @Override
-  public void destroy() throws Exception {
-    for (Map.Entry<NatsSubscriber, Dispatcher> entry : subscriptions.entrySet()) {
-      Dispatcher dispatcher = entry.getValue();
-      NatsSubscriber natsSubscriber = entry.getKey();
-      try {
-        dispatcher.unsubscribe(natsSubscriber.subject());
-        nc.closeDispatcher(dispatcher);
-      } catch (Exception e) {
-        log.error("NATS-100500: Failed to close {}", natsSubscriber.subject(), e);
-      }
+        return bean;
     }
 
-  }
 
-  private MethodHandle getMethodHandler(Method method) throws NoSuchMethodException, IllegalAccessException {
-    MethodHandles.Lookup lookup = MethodHandles.lookup();
-    MethodType mt = MethodType.methodType(void.class, Message.class);
-    return lookup.findVirtual(method.getDeclaringClass(), method.getName(), mt);
-  }
+    /*
+     * AnnotationUtils.getRepeatableAnnotations does not look at interfaces
+     */
+    private Set<NatsSubscriber> findSubscriberAnnotations(Method method) {
+        Set<NatsSubscriber> subscribers = new HashSet<>();
+        NatsSubscriber natsSubscriber = AnnotationUtils.findAnnotation(method, NatsSubscriber.class);
+        if (natsSubscriber != null) {
+            subscribers.add(natsSubscriber);
+        }
+        NatsSubscribers natsSubscribers = AnnotationUtils.findAnnotation(method, NatsSubscribers.class);
+        if (natsSubscribers != null) {
+            subscribers.addAll(Arrays.asList(natsSubscribers.value()));
+        }
+        return subscribers;
+    }
+
+    private void processNatsSubscriber(NatsSubscriber natsSubscriber, Method method, Object bean, String beanName) throws NoSuchMethodException, IllegalAccessException {
+        //use method handler instead of reflection for performance
+        MethodHandle methodHandler = getMethodHandler(method);
+        MessageHandler messageHandler = msg -> {
+            try {
+                methodHandler.invoke(bean, msg);
+                // method.invoke(bean, msg);
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        };
+        Dispatcher dispatcher;
+        String subject = natsSubscriber.subject();
+        String queueGroup = natsSubscriber.queueGroup();
+        // replace ${xxx} with environment value
+        if (subject.startsWith("${")) {
+            subject = resolver.resolveStringValue(subject);
+        }
+        if (queueGroup.startsWith("${")) {
+            queueGroup = resolver.resolveStringValue(queueGroup);
+        }
+        if ("".equals(queueGroup)) {
+            dispatcher = nc.createDispatcher(messageHandler);
+            dispatcher.subscribe(subject);
+        } else {
+            dispatcher = nc.createDispatcher(messageHandler);
+            dispatcher.subscribe(subject, queueGroup);
+        }
+        subscriptions.put(natsSubscriber, dispatcher);
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        for (Map.Entry<NatsSubscriber, Dispatcher> entry : subscriptions.entrySet()) {
+            Dispatcher dispatcher = entry.getValue();
+            NatsSubscriber natsSubscriber = entry.getKey();
+            try {
+                dispatcher.unsubscribe(natsSubscriber.subject());
+                nc.closeDispatcher(dispatcher);
+            } catch (Exception e) {
+                log.error("NATS-100500: Failed to close {}", natsSubscriber.subject(), e);
+            }
+        }
+
+    }
+
+    private MethodHandle getMethodHandler(Method method) throws NoSuchMethodException, IllegalAccessException {
+        MethodHandles.Lookup lookup = MethodHandles.lookup();
+        MethodType mt = MethodType.methodType(void.class, Message.class);
+        return lookup.findVirtual(method.getDeclaringClass(), method.getName(), mt);
+    }
 }
